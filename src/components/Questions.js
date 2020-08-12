db6d7c464690f8a347e794c7da40956e2a55c89d
MarkS <mark.smith1@fdmgroup.com>
2020-08-12 09:29:20 +0100

    Created java q's file and added questions.

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -23,7 +23,26 @@ class Questions extends Component {
         return (
             <div id="quiz">
                 <h2>{this.state.quiz.name}</h2>
-                
+                {questions.map(q =>
+                    <div key={q.id}>
+                        <div className="badge badge-info">Question {this.props.pager.index + 1} of {this.props.pager.count}.</div>
+                        <h3 className="font-weight-normal">{this.props.pager.index + 1}. <span>{q.name}</span></h3>
+                        <div className="row text-left options">
+                            {
+                                q.options.map(option =>
+                                    <div key={option.id} className="col-6">
+                                        <div className="option">
+                                            <label className="font-weight-normal" htmlFor={option.id}>
+                                                <input id={option.id} checked={option.selected} type="checkbox" onChange={() => this.onAnswer(q, option)} />
+                                                {option.name}
+                                            </label>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                )}
             </div>
         )
     }
